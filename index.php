5acff6b645ea2d328314da2a284de9ef1dbd370d
Kanstantsin <kanstantsin.litvin@gmail.com>
2018-07-12 18:09:52 +0300

    3rd commit

--- index.php
+++ index.php
@@ -11,18 +11,15 @@ if (isset($_GET['id'])){
 if (isset($_GET['action'])){
     $action = $_GET['action'];
     if($action == "done") {
-        //$db->exec("UPDATE task SET is_done='Выполнено' WHERE id = $id");
         $sth = $db->prepare("UPDATE task SET is_done='Выполнено' WHERE id = ? AND user_id = ?");
         $sth->execute(array($id, $user_id));
     }
     if($action == "delete") {
-        //$db->exec("DELETE FROM task WHERE id = $id");
         $sth = $db->prepare("DELETE FROM task WHERE id = ? AND user_id = ?");
         $sth->execute(array($id, $user_id));
     }
 }
 
-//$st = $db->query('SELECT * FROM task WHERE user_id = '.@$user_id.'');
 $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
         $sth->execute(array($user_id));
 ?>
@@ -87,7 +84,6 @@ $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
     </tr>
     </thead>
 
-
     <tbody>
     <?php
     if(isset($_POST['description'])) {
@@ -95,22 +91,8 @@ $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
         $is_done = "В процессе";
         date_default_timezone_set('Europe/Minsk');
         $date_added = date ('Y-m-d H:i:s');
-        //$user = $_SESSION['session_username'];
-        
-        //$user_id = $db->query("SELECT id FROM user WHERE login='".$user."'");
-        //$assigned_user = $_SESSION['session_username'];
-        //$assigned_user = $_POST['assigned_user'];
-        //$assigned_user_id = $db->query("SELECT id FROM user WHERE login='".$assigned_user."'");
-        //$thAuthor = $db->query("SELECT id FROM user WHERE login='".$session_username."'");
-
-        $userlist = $db->query("SELECT id, login FROM user");
-        $numrows = $userlist->fetchAll();
-        
-        //$rows = $db->exec("INSERT INTO task(id, description, is_done, date_added, user_id, assigned_user_id ) VALUES (null, '".@$thSave."','".@$thDone."','".@$thDate."', '".@$session_username."', '".@$thAuthor."')");
         $rows = $db->prepare("INSERT INTO task(id, description, is_done, date_added, user_id, assigned_user_id) VALUES (?, ?, ?, ?, ?, ?)");
         $rows->execute(array(null, $description, $is_done, $date_added, $user_id, null));
-        
-        //$st = $db->query('SELECT * FROM task WHERE user_id = '.@$user_id.'');
         $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
         $sth->execute(array($user_id));
     }
@@ -123,60 +105,65 @@ $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
                 <td><span style="color: goldenrod;"><?php echo $rows ['is_done'] ?></span></td>
                 <td><a href="?id=<?= $rows ['id'];?>&action=done">Выполнить</a>
                     <a href="?id=<?= $rows ['id'];?>&action=delete">Удалить</a></td>
-                <td>
-                    <?php $auth = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.user_id");
+                <td><?php $auth = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.user_id WHERE user_id = $user_id");
                           $auth->execute();
                           $author = $auth->fetch();
                           echo $author['login'];?>
-                    
-                    <!--?php if(isset($_POST['assigned_user'])) {
-                            echo $user = $_SESSION['session_username'];
-                          } else {
-                          echo $user = $_SESSION['session_username'];
-                          } ?-->
                 </td>
-                <td><?php if(isset($_POST['assigned_user_id'])) {
-                            $sth = $db->prepare("UPDATE task SET user_id = assigned_user_id  WHERE id = ? AND user_id = ?");
-                            $sth->execute(array($id, $user_id)); 
-                            $assign = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.assigned_user_id");
-                            $assign->execute();
-                            $assigned = $assign->fetch();
+                <td><?php if(isset($_POST['assigned_user'])) {
+                            $result = $_POST['assigned_user_id'];
+                            $result_explode = explode('_', $result);//получаем user_id и id задачи
+                            $stu = $db->prepare("UPDATE task SET assigned_user_id = ? WHERE id = ?");//изменяем assigned_user_id по id задачи
+                            $stu->execute(array($result_explode[0], $result_explode[1])); //подставляем user_id и id задачи
+                            $ass = $stu->fetch();
+                            $assigned_user = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.assigned_user_id WHERE task.id =" . $rows['id']);
+                            $assigned_user->execute();
+                            $assigned = $assigned_user->fetch();
                             echo $assigned['login'];
-                         
-                          ?>
-
-
-                        <?php //echo $assigned_user = $_POST['hero'];
-                          } else {
+                            } else {
                             echo $author['login'];
-                          }  ?></td>
-                <td>
-                    <form action="" method="post">
+                          }?>
+                </td>
+                <td><form action="" method="post">
                         <p><select name="assigned_user_id" size="5" multiple>
                                 <option disabled>Выберите пользователя</option>
                                 <?php
                                 $usernames = $db -> query("SELECT id, login FROM user");
-                                //$usernames = $userlist->fetch();
-                                foreach ($usernames as $value) {
-                                    foreach($value as $assigned_user['login']) {
-                                        echo '<option value="'.$assigned_user['id'].'">'.$assigned_user['login'].'</option>';
-                                    }
-                                
+                                $username = $usernames->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($username as $value) {
+                                    echo '<option value="'.$value['id'].'_'.$rows ['id'].'">'.$value['login'].'</option>';
                                 }?>
                             </select></p>
                         <p><input type="submit" name="assigned_user" value="Переложить ответственность"></p>
+                        
                     </form>
                 </td>
-
-
             </tr>
-
-    <?php }?>
-
-
+    <?php } ?>
     </tbody>
 </table>
+<!------------------------------------------------------------//////////////////////////////////////////////////////////////////////////////-->
+ <?php $assigned_user_id = $_SESSION['id'];
+if (isset($_GET['id'])){
+    $id = (int)$_GET['id'];
+}
 
+if (isset($_GET['action'])){
+    $action = $_GET['action'];
+    if($action == "done") {
+        $sth = $db->prepare("UPDATE task SET is_done='Выполнено' WHERE id = ? AND assigned_user_id = ?");
+        $sth->execute(array($id, $assigned_user_id));
+    }
+    if($action == "delete") {
+        $sth = $db->prepare("DELETE FROM task WHERE id = ? AND assigned_user_id = ?");
+        $sth->execute(array($id, $assigned_user_id));
+    }
+}
+
+$sth = $db->prepare("SELECT * FROM task WHERE assigned_user_id = ?");
+        $sth->execute(array($assigned_user_id));
+?>
+<h2><span>Также, посмотрите, что от Вас требуют другие люди:</span></h2>
 <table>
     <thead>
     <tr>
@@ -186,46 +173,66 @@ $sth = $db->prepare("SELECT * FROM task WHERE user_id = ?");
         <td><h4>Статус</h4></td>
         <td><h4>Действия</h4></td>
         <td><h4>Автор</h4></td>
+        <td><h4>Ответственный</h4></td>
+        
     </tr>
     </thead>
 
-    <h1>Cписок дел, порученных Вам другими пользователями:</h1>
-
     <tbody>
     <?php
-    if(!empty($_POST['description'])) {
-        $thSave = $_POST['description'];
-        $thDate = date ('Y-m-d H:i:s');
-        $thDone = "В процессе";
-        $session_username = $_SESSION['session_username'];
-        //$assigned_user = $_POST['assigned_user'];
-        //$thAuthor = $db->query("SELECT id FROM user WHERE login='".$session_username."'");
-        //$thAuthor = $db->query("SELECT id FROM assigned_user WHERE username='".$assigned_user."'");
-        $userlist = $db->query("SELECT id, login FROM user");
-        $numrows=$userlist->fetchAll();
-        //print_r($numrows);
-        $rows = $db->exec("INSERT INTO task(id, description, is_done, date_added, user_id, assigned_user_id ) VALUES (null, '".@$thSave."','".@$thDone."','".@$thDate."', '".@$session_username."', '".@thAuthor."')");
-        //$st = $db->query('SELECT * FROM task');
-    }
+    
+        /*$description = $_POST['description'];
+        $is_done = "В процессе";
+        date_default_timezone_set('Europe/Minsk');
+        $date_added = date ('Y-m-d H:i:s');
+        $rows = $db->prepare("INSERT INTO task(id, description, is_done, date_added, user_id, assigned_user_id) VALUES (?, ?, ?, ?, ?, ?)");
+        $rows->execute(array(null, $description, $is_done, $date_added, $user_id, null));*/
+        $sth = $db->prepare("SELECT * FROM task WHERE assigned_user_id = ?");
+        $sth->execute(array($assigned_user_id));
+    
 
     while ($rows = $sth->fetch()) { ?>
-        <tr>
-            <td><?php echo $rows ['id'] ?></td>
-            <td><?php echo $rows ['description'] ?></td>
-            <td><?php echo $rows ['date_added'] ?></td>
-            <td><span style="color: goldenrod;"><?php echo $rows ['is_done'] ?></span></td>
-            <td><a href="?id=<?= $rows ['id'];?>&action=done">Выполнить</a>
-                <a href="?id=<?= $rows ['id'];?>&action=delete">Удалить</a></td>
-            <td><?php echo $rows ['assigned_user_id'] ?></td>
-        </tr>
-
-
-    <?php }?>
-
-
+            <tr>
+                <td><?php echo $rows ['id'] ?></td>
+                <td><?php echo $rows ['description'] ?></td>
+                <td><?php echo $rows ['date_added'] ?></td>
+                <td><span style="color: goldenrod;"><?php echo $rows ['is_done'] ?></span></td>
+                <td><a href="?id=<?= $rows ['id'];?>&action=done">Выполнить</a>
+                    <a href="?id=<?= $rows ['id'];?>&action=delete">Удалить</a></td>
+                <td><?php $auth = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.user_id WHERE assigned_user_id = $assigned_user_id");
+                          $auth->execute();
+                          $author = $auth->fetch();
+                          echo $author['login'];?>
+                </td>
+                <td><?php /*if(isset($_POST['assigned_user'])) {
+                            $result = $_POST['assigned_user_id'];
+                            $result_explode = explode('_', $result);//получаем user_id и id задачи
+                            $stu = $db->prepare("UPDATE task SET assigned_user_id = ? WHERE id = ?");//изменяем assigned_user_id по id задачи
+                            $stu->execute(array($result_explode[0], $result_explode[1])); //подставляем user_id и id задачи
+                            $ass = $stu->fetch();*/
+                            $assigned_user = $db->prepare("SELECT user.login FROM `user` JOIN task ON user.id = task.assigned_user_id WHERE task.id =" . $rows['id']);
+                            $assigned_user->execute();
+                            $assigned = $assigned_user->fetch();
+                            echo $assigned['login'];
+                            /* } else {
+                            echo $author['login'];
+                          }*/?>
+                </td>
+                <!--td><form action="" method="post">
+                        <p><select name="assigned_user_id" size="5" multiple>
+                                <option disabled>Выберите пользователя</option>
+                                <!?php
+                                $usernames = $db -> query("SELECT id, login FROM user");
+                                $username = $usernames->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($username as $value) {
+                                    echo '<option value="'.$value['id'].'_'.$rows ['id'].'">'.$value['login'].'</option>';
+                                }?>
+                            </select></p>
+                        <p><input type="submit" name="assigned_user" value="Переложить ответственность"></p>
+                        
+                    </form>
+                </td-->
+            </tr>
+    <?php } ?>
     </tbody>
 </table>
-
-
-</body>
-</html>
